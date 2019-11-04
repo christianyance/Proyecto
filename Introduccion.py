1e21dec40daf7552e9d1c975bc06a4914aae2046
Jesus Rosillo <didiereros96@outlook.com>
2019-11-04 10:27:51 -0500

    parte calculo

--- Introduccion.py
+++ Introduccion.py
@@ -2,3 +2,40 @@ username = input("Ingrese nombre:")
 
 print("Bienvenido", username)
 print("Esta app")
+
+n = int(input("Ingrese el numero de puntos visados: "))
+c = int(input("Ingrese cota inicial: "))
+
+# solicitar los puntos visados de la vista atras
+puntos1 = []
+
+for i in range(n):
+    p1 = int(input("Ingrese vista atras: "))
+    puntos1.append(p1)
+    
+print(puntos1)
+
+# solicitar los puntos visados de la vista adelante
+puntos2 = []
+
+for i in range(n):
+    p2 = int(input("Ingrese vista adelante: "))
+    puntos2.append(p2)
+    
+print(puntos2)
+
+# calcular las alturas del nivel topografico
+alturas = []
+
+for j in range(n):
+    alturas.append(c + puntos1[j])
+
+print(alturas)
+
+# calcular las cotas
+#cotas = []
+
+#for j in range(n):
+    #cotas.append(alturas - puntos2)
+
+#print(cotas)
